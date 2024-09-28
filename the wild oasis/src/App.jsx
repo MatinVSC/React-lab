468f1959d0e9f0e11fcb86613cff2c443325c504
matin <matinvsc@gmail.com>
2024-09-28 20:13:18 +0330

    created the wild oasis and installed starter librarys

---
+++ the wild oasis/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import GlobalStyled from "./styles/GelobalStyled";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyled />
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter> 
+    </>
+  )
+}
+
+export default App
